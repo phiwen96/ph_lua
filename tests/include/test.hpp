6c81cb6e86ba2a567a8958a1a364abd119579c9d
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-04-19 11:48:22 +0200

    a

--- tests/include/test.hpp
+++ tests/include/test.hpp
@@ -9,36 +9,35 @@ using namespace std;
 auto run () -> int 
 {
     
-    return 0;
     LuaScript script(LUA_FILE);
-        float posX = script.get<float>("player.position.x");
-        float posY = script.get<float>("player.position.y");
-        std::string filename = script.get<std::string>("player.filename");
-        int hp = script.get<int>("player.HP");
-
-        std::cout<<"Position X = "<<posX<<", Y = "<<posY<<std::endl;
-        std::cout<<"Filename:"<<filename<<std::endl;
-        std::cout<<"HP:"<<hp<<std::endl;
-
-        // getting arrays
-        std::vector<int> v = script.getIntVector("array");
-        std::cout<<"Contents of array:";
-        for(std::vector<int>::iterator it = v.begin();
-                it != v.end();
-                it++) {
-                std::cout<<*it<<",";
-        }
-        std::cout<<std::endl;
-
-        // getting table keys:
-        std::vector<std::string> keys = script.getTableKeys("player");
-        std::cout<<"Keys of [player] table:";
-        for(std::vector<std::string>::iterator it = keys.begin();
-                it != keys.end();
-                it++) {
-                std::cout<<*it<<",";
-        }
-        std::cout<<std::endl;
- 
+    float posX = script.get<float>("player.position.x");
+    float posY = script.get<float>("player.position.y");
+    std::string filename = script.get<std::string>("player.filename");
+    int hp = script.get<int>("player.HP");
+    
+    std::cout<<"Position X = "<<posX<<", Y = "<<posY<<std::endl;
+    std::cout<<"Filename:"<<filename<<std::endl;
+    std::cout<<"HP:"<<hp<<std::endl;
+    
+    // getting arrays
+    std::vector<int> v = script.getIntVector("array");
+    std::cout<<"Contents of array:";
+    for(std::vector<int>::iterator it = v.begin();
+        it != v.end();
+        it++) {
+        std::cout<<*it<<",";
+    }
+    std::cout<<std::endl;
+    
+    // getting table keys:
+    std::vector<std::string> keys = script.getTableKeys("player");
+    std::cout<<"Keys of [player] table:";
+    for(std::vector<std::string>::iterator it = keys.begin();
+        it != keys.end();
+        it++) {
+        std::cout<<*it<<",";
+    }
+    std::cout<<std::endl;
+    
     return 0;
 }
